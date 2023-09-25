ef0d836cb38d2f68a61ca4bd438c78e6be11f1c7
Lewis Bolaton <lewisbolaton@gmail.com>
2023-09-25 09:58:02 +0800

    Initial tab layout. Changed typeface

---
+++ components/Tabs.vue
@@ -0,0 +1,47 @@
+<template>
+	<ul>
+    <li><span class="material-icons">directions_bus</span>EDSA<br/>Carousel</li>
+    <li><span class="material-icons">tram</span>LRT-1</li>
+    <li><span class="material-icons">tram</span>MRT-3</li>
+    <li><span class="material-icons">tram</span>LRT-2</li>
+	</ul>
+</template>
+
+<style scoped>
+  /* Remove default styling */
+  ul, li {
+    margin: 0;
+    padding: 0;
+  }
+
+  li {
+    list-style: none;
+  }
+
+  /* Declare custom styling */
+  ul {
+    display: flex;
+    font-size: 0.85rem;
+    gap: 0.67rem;
+    margin: 0 0.67rem;
+  }
+
+  li {
+    width: 25%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    padding: 0.5em 0;
+    border-radius: 8px 8px 0 0;
+  }
+
+  li:first-child {
+    font-size: 0.85em;
+  }
+
+  li:hover {
+    cursor: pointer;
+    background-color: #bbbbbb;
+  }
+</style>
